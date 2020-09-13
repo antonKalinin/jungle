b4c6e4e43144ae074fcb9e4a3c78c1068ef05cad
Anton Kalinin <hurricanebox@gmail.com>
2020-09-13 22:10:44 +0300

    Basic of jumping, experimenting with map

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,14 @@ use argh::FromArgs;
 use bevy::{prelude::*, render::pass::ClearColor};
 use std::collections::HashMap;
 
+const AXIS_X: usize = 0;
+const AXIS_Y: usize = 1;
+const BG_WIDTH: f32 = 384.0;
+const BG_HEIGHT: f32 = 216.0;
+const GRAVITY: f32 = 32.0;
+const PLAYER_HORIZONTAL_SPEED: f32 = 8.0;
+const PLAYER_INITIAL_VERTICAL_SPEED: f32 = 16.0;
+
 #[derive(FromArgs)]
 #[argh(description = "Jungle game settings")]
 struct Options {
@@ -21,21 +29,66 @@ struct Background {
     acceleration: f32,
 }
 
-struct Player {
-    velocity: f32,
+trait AABB {
+    fn collides(&self, other: &impl AABB) -> bool {
+        let self_position = self.position();
+        let self_size = self.size();
+        let other_position = other.position();
+        let other_size = other.size();
+
+        if self_position[AXIS_X] - other_position[AXIS_X]
+            > self_size[AXIS_X] / 2.0 + other_size[AXIS_X] / 2.0
+        {
+            return false;
+        }
+
+        if self_position[AXIS_Y] - other_position[AXIS_Y]
+            > self_size[AXIS_Y] / 2.0 + other_size[AXIS_Y] / 2.0
+        {
+            return false;
+        }
+
+        true
+    }
+    fn position(&self) -> Vec2;
+    fn size(&self) -> Vec2;
 }
 
-const BG_WIDTH: f32 = 384.0;
-const BG_HEIGHT: f32 = 216.0;
-const PLAYER_SPEED: f32 = 8.0;
+struct Player {
+    scale: f32,
+    position: Vec2,
+    size: Vec2,
+    velocity: Vec2,
+}
 
-#[derive(Clone, Properties, Debug, Default)]
-pub struct Sprite {
-    pub atlas_handle: Handle<TextureAtlas>,
+impl AABB for Player {
+    fn position(&self) -> Vec2 {
+        self.position.clone()
+    }
+
+    fn size(&self) -> Vec2 {
+        self.size.clone()
+    }
+}
+
+struct Tile {
+    scale: f32,
+    position: Vec2,
+    size: Vec2,
+}
+
+impl AABB for Tile {
+    fn position(&self) -> Vec2 {
+        self.position.clone()
+    }
+
+    fn size(&self) -> Vec2 {
+        self.size.clone()
+    }
 }
 
 pub struct Sprites {
-    library: HashMap<String, Sprite>,
+    library: HashMap<String, Handle<TextureAtlas>>,
 }
 
 impl Sprites {
@@ -46,12 +99,10 @@ impl Sprites {
     }
 
     pub fn add(&mut self, key: String, atlas_handle: Handle<TextureAtlas>) {
-        let sprite = Sprite { atlas_handle };
-
-        self.library.insert(key, sprite);
+        self.library.insert(key, atlas_handle);
     }
 
-    pub fn get(&self, key: &str) -> Option<&Sprite> {
+    pub fn get(&self, key: &str) -> Option<&Handle<TextureAtlas>> {
         self.library.get(key)
     }
 }
@@ -70,6 +121,7 @@ fn startup(
     mut texture_atlases: ResMut<Assets<TextureAtlas>>,
 ) {
     let scale = options.scale as f32;
+    let bottom = scale * (-BG_HEIGHT / 2.0 + 20.0);
 
     // Add the game's entities to our world
     commands.spawn(Camera2dComponents::default());
@@ -105,46 +157,99 @@ fn startup(
     let idle_texture = textures.get(&idle_texture_handle).unwrap();
     let idle_texture_atlas = TextureAtlas::from_grid(idle_texture_handle, idle_texture.size, 12, 1);
 
+    let jump_texture_handle = asset_server
+        .load_sync(&mut textures, "assets/player/jump.png")
+        .unwrap();
+    let jump_texture = textures.get(&jump_texture_handle).unwrap();
+    let jump_texture_atlas = TextureAtlas::from_grid(jump_texture_handle, jump_texture.size, 1, 1);
+
+    let tile_texture_handle = asset_server
+        .load_sync(&mut textures, "assets/tileset.png")
+        .unwrap();
+    let tile_texture = textures.get(&tile_texture_handle).unwrap();
+    let tile_texture_atlas =
+        TextureAtlas::from_grid(tile_texture_handle, tile_texture.size, 48, 23);
+
     let run_atlas_handle = texture_atlases.add(run_texture_atlas);
     let idle_atlas_handle = texture_atlases.add(idle_texture_atlas);
+    let jump_atlas_handle = texture_atlases.add(jump_texture_atlas);
+    let tile_atlas_handle = texture_atlases.add(tile_texture_atlas);
 
     commands
         .spawn(SpriteSheetComponents {
             scale: Scale(scale),
-            translation: Translation::new(0.0, -BG_HEIGHT - 100.0, 10.0),
+            translation: Translation::new(0.0, bottom, 10.0),
             texture_atlas: idle_atlas_handle.clone(),
             ..Default::default()
         })
-        .with(Player { velocity: 0.0 })
+        .with(Player {
+            scale,
+            size: Vec2::new(34.0, 20.0),
+            position: Vec2::new(0.0, 0.0),
+            velocity: Vec2::new(0.0, 0.0),
+        })
         .with(Timer::from_seconds(0.1, true));
 
     let mut sprites = Sprites::new();
 
     sprites.add("player_run".to_string(), run_atlas_handle);
     sprites.add("player_idle".to_string(), idle_atlas_handle);
+    sprites.add("player_jump".to_string(), jump_atlas_handle);
 
     commands.insert_resource(sprites);
+
+    commands
+        .spawn(SpriteSheetComponents {
+            scale: Scale(scale),
+            sprite: TextureAtlasSprite::new(101),
+            texture_atlas: tile_atlas_handle.clone(),
+            translation: Translation::new(100.0, bottom - 10.0, 11.0),
+            ..Default::default()
+        })
+        .with(Tile {
+            scale,
+            size: Vec2::new(16.0, 16.0),
+            position: Vec2::new(10.0, 0.0),
+        });
 }
 
 fn movement(
+    time: Res<Time>,
+    options: Res<Options>,
     keyboard_input: Res<Input<KeyCode>>,
-    mut player_query: Query<(&mut Player, &mut Rotation)>,
+    mut player_query: Query<(&mut Player, &mut Translation, &mut Rotation)>,
 ) {
-    for (mut player, mut rotation) in &mut player_query.iter() {
+    let bottom = options.scale as f32 * (-BG_HEIGHT / 2.0 + 20.0);
+
+    for (mut player, mut translation, mut rotation) in &mut player_query.iter() {
         if keyboard_input.pressed(KeyCode::Left) {
-            player.velocity = PLAYER_SPEED;
+            player.velocity[AXIS_X] = PLAYER_HORIZONTAL_SPEED;
             *rotation = Rotation(Quat::from_rotation_y(std::f32::consts::PI));
         }
 
         if keyboard_input.pressed(KeyCode::Right) {
-            player.velocity = -PLAYER_SPEED;
+            player.velocity[AXIS_X] = -PLAYER_HORIZONTAL_SPEED;
             *rotation = Rotation(Quat::from_rotation_y(0.0));
+        }
+
+        if keyboard_input.just_released(KeyCode::Up) {
+            player.velocity[AXIS_Y] = PLAYER_INITIAL_VERTICAL_SPEED;
         }
 
         if keyboard_input.just_released(KeyCode::Right)
             || keyboard_input.just_released(KeyCode::Left)
         {
-            player.velocity = 0.0;
+            player.velocity[AXIS_X] = 0.0;
+        }
+
+        if player.velocity[AXIS_Y] != 0.0 {
+            translation[AXIS_Y] = translation[AXIS_Y] + player.velocity[AXIS_Y];
+        }
+
+        if translation[AXIS_Y] > bottom {
+            player.velocity[AXIS_Y] -= GRAVITY * time.delta_seconds;
+        } else {
+            player.velocity[AXIS_Y] = 0.0;
         }
     }
 }
@@ -164,13 +269,19 @@ fn animation(
     let scale = options.scale as f32;
 
     for (player, timer, mut sprite, mut texture_atlas_handle) in &mut player_query.iter() {
-        if player.velocity != 0.0 {
-            if let Some(sprite) = sprites.get("player_run") {
-                *texture_atlas_handle = sprite.atlas_handle;
+        if player.velocity[AXIS_X] != 0.0 {
+            if let Some(sprite_handle) = sprites.get("player_run") {
+                *texture_atlas_handle = *sprite_handle;
             }
         } else {
-            if let Some(sprite) = sprites.get("player_idle") {
-                *texture_atlas_handle = sprite.atlas_handle;
+            if let Some(sprite_handle) = sprites.get("player_idle") {
+                *texture_atlas_handle = *sprite_handle;
+            }
+        }
+
+        if player.velocity[AXIS_Y] != 0.0 {
+            if let Some(sprite_handle) = sprites.get("player_jump") {
+                *texture_atlas_handle = *sprite_handle;
             }
         }
 
@@ -180,7 +291,7 @@ fn animation(
         }
 
         for (background, mut translation) in &mut bg_query.iter() {
-            let step = player.velocity * background.acceleration;
+            let step = player.velocity[AXIS_X] * background.acceleration;
 
             *translation.0.x_mut() += step;
 
---
+++ twitter/twits.md
@@ -0,0 +1,12 @@
+Here is lazy thread about how I am learning #rust by making platformer game with @BevyEngine. Lets go!
+#bevyengine #rustgamedev #rustlang
+
+Short intro: being a JS/TS web dev for almost all my career last year I decided to dive into Rust. I though it would be fun: most loved lang 3 years in a row, proper types, new concepts. Naive me)
+
+Having kind of huge cemetery of pet projects I knew that if I start creating assets by my own, it would be another headstone, so I found this super nice asset pack https://jesse-m.itch.io/jungle-pack by @Jsf23art.
+
+There is everything you need to write your first platformer as I do: parallax background, character animations, all kind of tiles. Definitely worths a couple of bucks paid! Not an ad! (it is)
+
+Another
+
+(to have something more lowlevel in my lang portfolio, to feel how to work with proper type system)
